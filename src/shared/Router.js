ec1596b6867e2d05ab1a086bdee2716451ee0535
Your Name <you@your-domain.com>
2024-02-20 16:15:28 +0900

    Router 셋업

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "pages/Home";
 import DetailFanletter from "pages/DetailFanletter";
+import LoginPage from "pages/LoginPage";
+import ProfilePage from "pages/ProfilePage";
 
 const Router = () => {
   //팬레터 리스트
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="home/" element={<Home />} />
+        <Route path="profile/:id" element={<ProfilePage />} />
         <Route path="detail/:id" element={<DetailFanletter />} />
       </Routes>
     </BrowserRouter>
